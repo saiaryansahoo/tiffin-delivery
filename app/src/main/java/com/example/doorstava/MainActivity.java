47969bb0e1c163e1a598e5ce98ece0f50542037b
aryansahoo0912 <jeetan.bdk@gmail.com>
2022-08-25 00:47:11 +0530

    initial commit

--- app/src/main/java/com/example/doorstava/MainActivity.java
+++ app/src/main/java/com/example/doorstava/MainActivity.java
@@ -3,13 +3,27 @@ package com.example.doorstava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceStates) {
         super.onCreate(savedInstanceStates);
         setContentView(R.layout.activity_main);
+        button=(Button) findViewById(R.id.startbtn);
+        button.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,loginpage.class);
+                startActivity(intent);
+            }
+        }));
+
+
 
 
     }
--- app/src/main/java/com/example/doorstava/homescreen.java
+++ app/src/main/java/com/example/doorstava/homescreen.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-
-public class loginpage extends AppCompatActivity {
+public class homescreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-            }
-        }
-
-
+        setContentView(R.layout.activity_homescreen);
+    }
+}
