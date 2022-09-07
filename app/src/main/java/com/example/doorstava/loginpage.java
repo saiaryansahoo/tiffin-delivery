c8029844da4b29213df12dd6931231e929cb8302
aryansahoo0912 <jeetan.bdk@gmail.com>
2022-09-07 22:16:47 +0530

    initial commit

--- app/src/main/java/com/example/doorstava/loginpage.java
+++ app/src/main/java/com/example/doorstava/loginpage.java
@@ -3,15 +3,27 @@ package com.example.doorstava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 
 public class loginpage extends AppCompatActivity {
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginpage);
+        button = (Button) findViewById(R.id.log);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(loginpage.this, homescreen.class);
+                startActivity(intent);
 
             }
-        }
+        });
 
-
+    }
+}
